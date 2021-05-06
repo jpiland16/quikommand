1168205f6420ab52a2d46493d3f65491bd800634
Jonathan Piland <jonathan.piland@duke.edu>
2021-05-06 11:58:03 -0400

    move help to .bat file

---
+++ help.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Welcome! Begin typing and use UP/DOWN arrows to move and ENTER to select.
+echo Alternatively, press CTRL and the given NUMPAD key (to the left of the entry)
+echo to select and execute a command.&echo.&echo Type 'about' and press ENTER for more information.
+echo.
+echo Press ESC at any time to exit.
+echo.
+pause
+quikommand.exe
